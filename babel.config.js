992c06d73c77964e9e3533b1bd488d99f8f1fb56
pategood <zxc2283879877@outlook.com>
2021-05-18 23:55:38 +0800

    无

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,8 @@
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
 
-  configureWebpack:{
-    // 配置别名
-    resolve:{
-      alias: {
-        '@': resolve('src'),
-      }
-    }
-  },
+
 }
